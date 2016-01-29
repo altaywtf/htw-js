6a378c2f39bd84b4300379ab9e9d203d00d1d81d
Altay Aydemir <altayaydemir@gmail.com>
2016-01-29 20:59:46 +0200

    survivejs, alt implementation

--- 3/survive-js/kanban_app/app/components/App.jsx
+++ 3/survive-js/kanban_app/app/components/App.jsx
@@ -1,30 +1,27 @@
-import uuid from 'node-uuid';
 import React from 'react';
-
 import Notes from './Notes';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
-
-		this.state = {
-			notes: [
-				{
-				id: uuid.v4(),
-				task: 'Learn Webpack'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		};
+		this.state = NoteStore.getState();
 	}
+
+	componentDidMount() {
+		NoteStore.listen(this.storeChanged);
+	}
+
+	componentWillUnmount() {
+		NoteStore.unlisten(this.storeChanged);
+	}
+
+	storeChanged = (state) => {
+		this.setState(state);
+	};
 
 	render() {
 		const notes = this.state.notes;
@@ -37,29 +34,16 @@ export default class App extends React.Component {
 		);
 	}
 
-	addNote = () => {
-		this.setState({
-      notes: this.state.notes.concat([{
-      	id: uuid.v4(),
-      	task: 'New task'
-      }])
-    });
-  };
+	deleteNote(id) {
+		NoteActions.delete(id);
+	}
 
-  editNote = (id, task) => {
-  	const notes = this.state.notes.map(note => {
-  		if(note.id === id && task) {
-  			note.task = task;
-  		}
-  		return note;
-  	});
+	addNote() {
+		NoteActions.create({ task: 'New Task'});
+	}
 
-  	this.setState({notes});
-  };
-
-  deleteNote = (id) => {
-  	this.setState({
-  		notes: this.state.notes.filter(note => note.id !== id)
-  	});
-  };
+	editNote(id, task) {
+		NoteActions.update({id, task});
+	}
+	
 }
