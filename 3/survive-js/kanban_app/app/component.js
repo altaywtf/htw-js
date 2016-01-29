ff7d5433cfb5312091f7cef187271d7a3b92fddc
Altay Aydemir <altayaydemir@gmail.com>
2016-01-29 13:41:27 +0200

    setting up for survivejs

---
+++ 3/survive-js/kanban_app/app/component.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+	var element = document.createElement('h1');
+
+	element.innerHTML = 'Hello World!';
+
+	return element;
+}
---
+++ 3/survive-js/kanban_app/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
+};
+
+const common = {
+	entry: PATHS.app,
+
+	resolve: {
+		extensions: ['', 'js', 'jsx']
+	}
+
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	}
+		
+	module: {
+    	loaders: [
+      		{
+		        test: /\.css$/,
+		        loaders: ['style', 'css'],
+		        include: PATHS.app
+	      	},
+
+	      	{
+	      		test: /\.jsx?$/,
+	      		loaders: ['babel?cacheDirectory'],
+	      		include: PATHS.app
+	      	}
+    	]
+  	}
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      devtool: 'eval-source-map',
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
